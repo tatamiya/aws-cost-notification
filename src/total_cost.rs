5d5d6bf97588d726159465f05f55bd0c62c5cc92
tatamiya <tamyuji1002@gmail.com>
2021-07-22 13:18:55 +0900

    clean: refactor tests for cost response parser

--- src/total_cost.rs
+++ src/total_cost.rs
@@ -63,7 +63,7 @@ fn parse_service_costs(res: GetCostAndUsageResponse) -> Vec<ParsedServiceCost> {
 }
 
 #[cfg(test)]
-mod total_cost_tests {
+mod tests {
     use super::*;
     use rusoto_ce::*;
     use std::collections::HashMap;
@@ -76,50 +76,6 @@ mod total_cost_tests {
         let actual_parsed_date = parse_timestamp_into_local_date(input_timestamp).unwrap();
         assert_eq!(expected_parsed_date, actual_parsed_date);
     }
-
-    #[test]
-    fn parse_total_cost_correctly() {
-        let mut input_total_cost = HashMap::new();
-        input_total_cost.insert(
-            String::from("AmortizedCost"),
-            MetricValue {
-                amount: Some(String::from("1234.56")),
-                unit: Some(String::from("USD")),
-            },
-        );
-        let input_response = GetCostAndUsageResponse {
-            dimension_value_attributes: None,
-            group_definitions: None,
-            next_page_token: None,
-            results_by_time: Some(vec![ResultByTime {
-                estimated: Some(false),
-                groups: None,
-                time_period: Some(DateInterval {
-                    start: String::from("2021-07-01"),
-                    end: String::from("2021-07-18"),
-                }),
-                total: Some(input_total_cost),
-            }]),
-        };
-
-        let expected_parsed_total_cost = ParsedTotalCost {
-            start_date: Local.ymd(2021, 7, 1),
-            end_date: Local.ymd(2021, 7, 18),
-            cost: 1234.56,
-            unit: String::from("USD"),
-        };
-
-        let actual_parsed_total_cost = parse_total_cost(input_response);
-
-        assert_eq!(expected_parsed_total_cost, actual_parsed_total_cost);
-    }
-}
-
-#[cfg(test)]
-mod service_cost_tests {
-    use super::*;
-    use rusoto_ce::*;
-    use std::collections::HashMap;
 
     struct InputServiceCost {
         service_name: String,
@@ -171,6 +127,29 @@ mod service_cost_tests {
                 total: Some(total),
             }]),
         }
+    }
+
+    #[test]
+    fn parse_total_cost_correctly() {
+        let input_response: GetCostAndUsageResponse = prepare_sample_response(
+            Some(DateInterval {
+                start: String::from("2021-07-01"),
+                end: String::from("2021-07-18"),
+            }),
+            Some(String::from("1234.56")),
+            None,
+        );
+
+        let expected_parsed_total_cost = ParsedTotalCost {
+            start_date: Local.ymd(2021, 7, 1),
+            end_date: Local.ymd(2021, 7, 18),
+            cost: 1234.56,
+            unit: String::from("USD"),
+        };
+
+        let actual_parsed_total_cost = parse_total_cost(input_response);
+
+        assert_eq!(expected_parsed_total_cost, actual_parsed_total_cost);
     }
 
     #[test]
