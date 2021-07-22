35a35b5a93c89c946b06cfee04293bc3be6d98f3
tatamiya <tamyuji1002@gmail.com>
2021-07-22 20:17:22 +0900

    clean: DateRange built by method

--- src/date_range.rs
+++ src/date_range.rs
@@ -5,21 +5,22 @@ pub struct DateRange {
     pub start_date: Date<Local>,
     pub end_date: Date<Local>,
 }
+impl DateRange {
+    fn new(reporting_date: Date<Local>) -> Self {
+        let first_day_of_month = reporting_date.with_day(1).unwrap();
 
-pub fn get_date_range(target_date: Date<Local>) -> DateRange {
-    let first_day_of_month = target_date.with_day(1).unwrap();
+        let start_date: Date<Local>;
+        if reporting_date == first_day_of_month {
+            // First day of the previous month
+            start_date = first_day_of_month.pred().with_day(1).unwrap();
+        } else {
+            start_date = first_day_of_month;
+        }
 
-    let start_date: Date<Local>;
-    if target_date == first_day_of_month {
-        // First day of the previous month
-        start_date = first_day_of_month.pred().with_day(1).unwrap();
-    } else {
-        start_date = first_day_of_month;
-    }
-
-    DateRange {
-        start_date: start_date,
-        end_date: target_date,
+        DateRange {
+            start_date: start_date,
+            end_date: reporting_date,
+        }
     }
 }
 
@@ -37,7 +38,7 @@ mod date_range_tests {
             end_date: Local.ymd(2021, 7, 18),
         };
 
-        let actual_date_range = get_date_range(input_date);
+        let actual_date_range = DateRange::new(input_date);
 
         assert_eq!(expected_date_range, actual_date_range);
     }
@@ -51,7 +52,7 @@ mod date_range_tests {
             end_date: Local.ymd(2021, 7, 1),
         };
 
-        let actual_date_range = get_date_range(input_date);
+        let actual_date_range = DateRange::new(input_date);
 
         assert_eq!(expected_date_range, actual_date_range);
     }
