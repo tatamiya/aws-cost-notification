a4b86a9f0eff65de88e581ef0f1cdf1e16671ada
tatamiya <tamyuji1002@gmail.com>
2021-07-23 13:39:31 +0900

    clean: sort functions and structs

--- src/cost_explorer.rs
+++ src/cost_explorer.rs
@@ -5,27 +5,6 @@ use rusoto_ce::{GetCostAndUsageRequest, GetCostAndUsageResponse, Group, GroupDef
 use crate::cost_usage_client::GetCostAndUsage;
 use crate::date_range::ReportDateRange;
 
-fn build_cost_and_usage_request(
-    report_date_range: ReportDateRange,
-    is_total: bool,
-) -> GetCostAndUsageRequest {
-    let group_by: Option<Vec<GroupDefinition>> = match is_total {
-        true => None,
-        false => Some(vec![GroupDefinition {
-            type_: Some("DIMENSION".to_string()),
-            key: Some("SERVICE".to_string()),
-        }]),
-    };
-    GetCostAndUsageRequest {
-        filter: None,
-        granularity: "MONTHLY".to_string(),
-        group_by: group_by,
-        metrics: vec!["AmortizedCost".to_string()],
-        next_page_token: None,
-        time_period: report_date_range.as_date_interval(),
-    }
-}
-
 struct CostExplorerService<T: GetCostAndUsage> {
     client: T,
     report_date_range: ReportDateRange,
@@ -51,6 +30,27 @@ impl<T: GetCostAndUsage> CostExplorerService<T> {
             build_cost_and_usage_request(self.report_date_range, false);
         let res = block_on(self.client.get_cost_and_usage(request)).unwrap();
         ParsedServiceCost::from_response(&res)
+    }
+}
+
+fn build_cost_and_usage_request(
+    report_date_range: ReportDateRange,
+    is_total: bool,
+) -> GetCostAndUsageRequest {
+    let group_by: Option<Vec<GroupDefinition>> = match is_total {
+        true => None,
+        false => Some(vec![GroupDefinition {
+            type_: Some("DIMENSION".to_string()),
+            key: Some("SERVICE".to_string()),
+        }]),
+    };
+    GetCostAndUsageRequest {
+        filter: None,
+        granularity: "MONTHLY".to_string(),
+        group_by: group_by,
+        metrics: vec!["AmortizedCost".to_string()],
+        next_page_token: None,
+        time_period: report_date_range.as_date_interval(),
     }
 }
 
@@ -236,6 +236,113 @@ mod test_helpers {
 }
 
 #[cfg(test)]
+mod test_cost_explorer_service {
+    use super::test_helpers::*;
+    use super::*;
+    use crate::date_range::ReportDateRange;
+    use chrono::{Local, TimeZone};
+
+    #[test]
+    fn request_total_cost_correctly() {
+        let client_stub = CostAndUsageClientStub {
+            service_costs: None,
+            total_cost: Some(String::from("1234.56")),
+        };
+        let report_date_range = ReportDateRange::new(Local.ymd(2021, 7, 23));
+        let explorer = CostExplorerService::new(client_stub, report_date_range);
+
+        let expected_total_cost = ParsedTotalCost {
+            start_date: Local.ymd(2021, 7, 1),
+            end_date: Local.ymd(2021, 7, 23),
+            cost: 1234.56,
+            unit: String::from("USD"),
+        };
+
+        let actual_total_cost = explorer.request_total_cost();
+
+        assert_eq!(expected_total_cost, actual_total_cost);
+    }
+
+    #[test]
+    fn request_service_costs_correctly() {
+        let client_stub = CostAndUsageClientStub {
+            service_costs: Some(vec![
+                InputServiceCost::new("Amazon Simple Storage Service", "1234.56"),
+                InputServiceCost::new("Amazon Elastic Compute Cloud", "31415.92"),
+            ]),
+            total_cost: None,
+        };
+        let report_date_range = ReportDateRange::new(Local.ymd(2021, 7, 23));
+        let explorer = CostExplorerService::new(client_stub, report_date_range);
+
+        let expected_service_costs = vec![
+            ParsedServiceCost {
+                service_name: String::from("Amazon Simple Storage Service"),
+                cost: 1234.56,
+                unit: String::from("USD"),
+            },
+            ParsedServiceCost {
+                service_name: String::from("Amazon Elastic Compute Cloud"),
+                cost: 31415.92,
+                unit: String::from("USD"),
+            },
+        ];
+
+        let actual_service_costs = explorer.request_service_costs();
+
+        assert_eq!(expected_service_costs, actual_service_costs);
+    }
+}
+
+#[cfg(test)]
+mod test_build_request {
+    use super::*;
+    use crate::date_range::ReportDateRange;
+    use chrono::{Local, TimeZone};
+    use rusoto_ce::DateInterval;
+
+    #[test]
+    fn build_total_cost_request_correctly() {
+        let input_date_range = ReportDateRange::new(Local.ymd(2021, 7, 23));
+        let expected_request = GetCostAndUsageRequest {
+            filter: None,
+            granularity: String::from("MONTHLY"),
+            group_by: None,
+            metrics: vec![String::from("AmortizedCost")],
+            next_page_token: None,
+            time_period: DateInterval {
+                start: "2021-07-01".to_string(),
+                end: "2021-07-23".to_string(),
+            },
+        };
+        let actual_request = build_cost_and_usage_request(input_date_range, true);
+        assert_eq!(expected_request, actual_request);
+    }
+
+    #[test]
+    fn build_service_costs_request_correctly() {
+        let input_date_range = ReportDateRange::new(Local.ymd(2021, 7, 23));
+        let expected_request = GetCostAndUsageRequest {
+            filter: None,
+            granularity: String::from("MONTHLY"),
+            group_by: Some(vec![GroupDefinition {
+                type_: Some("DIMENSION".to_string()),
+                key: Some("SERVICE".to_string()),
+            }]),
+            metrics: vec![String::from("AmortizedCost")],
+            next_page_token: None,
+            time_period: DateInterval {
+                start: "2021-07-01".to_string(),
+                end: "2021-07-23".to_string(),
+            },
+        };
+        let actual_request = build_cost_and_usage_request(input_date_range, false);
+
+        assert_eq!(expected_request, actual_request);
+    }
+}
+
+#[cfg(test)]
 mod test_parsers {
     use super::test_helpers::*;
     use super::*;
@@ -298,112 +405,5 @@ mod test_parsers {
         let actual_parsed_service_costs = ParsedServiceCost::from_response(&input_response);
 
         assert_eq!(expected_parsed_service_costs, actual_parsed_service_costs);
-    }
-}
-
-#[cfg(test)]
-mod test_build_request {
-    use super::*;
-    use crate::date_range::ReportDateRange;
-    use chrono::{Local, TimeZone};
-    use rusoto_ce::DateInterval;
-
-    #[test]
-    fn build_total_cost_request_correctly() {
-        let input_date_range = ReportDateRange::new(Local.ymd(2021, 7, 23));
-        let expected_request = GetCostAndUsageRequest {
-            filter: None,
-            granularity: String::from("MONTHLY"),
-            group_by: None,
-            metrics: vec![String::from("AmortizedCost")],
-            next_page_token: None,
-            time_period: DateInterval {
-                start: "2021-07-01".to_string(),
-                end: "2021-07-23".to_string(),
-            },
-        };
-        let actual_request = build_cost_and_usage_request(input_date_range, true);
-        assert_eq!(expected_request, actual_request);
-    }
-
-    #[test]
-    fn build_service_costs_request_correctly() {
-        let input_date_range = ReportDateRange::new(Local.ymd(2021, 7, 23));
-        let expected_request = GetCostAndUsageRequest {
-            filter: None,
-            granularity: String::from("MONTHLY"),
-            group_by: Some(vec![GroupDefinition {
-                type_: Some("DIMENSION".to_string()),
-                key: Some("SERVICE".to_string()),
-            }]),
-            metrics: vec![String::from("AmortizedCost")],
-            next_page_token: None,
-            time_period: DateInterval {
-                start: "2021-07-01".to_string(),
-                end: "2021-07-23".to_string(),
-            },
-        };
-        let actual_request = build_cost_and_usage_request(input_date_range, false);
-
-        assert_eq!(expected_request, actual_request);
-    }
-}
-
-#[cfg(test)]
-mod test_cost_explorer_service {
-    use super::test_helpers::*;
-    use super::*;
-    use crate::date_range::ReportDateRange;
-    use chrono::{Local, TimeZone};
-
-    #[test]
-    fn request_total_cost_correctly() {
-        let client_stub = CostAndUsageClientStub {
-            service_costs: None,
-            total_cost: Some(String::from("1234.56")),
-        };
-        let report_date_range = ReportDateRange::new(Local.ymd(2021, 7, 23));
-        let explorer = CostExplorerService::new(client_stub, report_date_range);
-
-        let expected_total_cost = ParsedTotalCost {
-            start_date: Local.ymd(2021, 7, 1),
-            end_date: Local.ymd(2021, 7, 23),
-            cost: 1234.56,
-            unit: String::from("USD"),
-        };
-
-        let actual_total_cost = explorer.request_total_cost();
-
-        assert_eq!(expected_total_cost, actual_total_cost);
-    }
-
-    #[test]
-    fn request_service_costs_correctly() {
-        let client_stub = CostAndUsageClientStub {
-            service_costs: Some(vec![
-                InputServiceCost::new("Amazon Simple Storage Service", "1234.56"),
-                InputServiceCost::new("Amazon Elastic Compute Cloud", "31415.92"),
-            ]),
-            total_cost: None,
-        };
-        let report_date_range = ReportDateRange::new(Local.ymd(2021, 7, 23));
-        let explorer = CostExplorerService::new(client_stub, report_date_range);
-
-        let expected_service_costs = vec![
-            ParsedServiceCost {
-                service_name: String::from("Amazon Simple Storage Service"),
-                cost: 1234.56,
-                unit: String::from("USD"),
-            },
-            ParsedServiceCost {
-                service_name: String::from("Amazon Elastic Compute Cloud"),
-                cost: 31415.92,
-                unit: String::from("USD"),
-            },
-        ];
-
-        let actual_service_costs = explorer.request_service_costs();
-
-        assert_eq!(expected_service_costs, actual_service_costs);
     }
 }
