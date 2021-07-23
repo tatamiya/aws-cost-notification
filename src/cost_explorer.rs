929290314c8b0d6269111e81780110d7b84b1b4b
tatamiya <tamyuji1002@gmail.com>
2021-07-23 11:05:40 +0900

    add: define a restricted trait and a wrapper for client

--- src/cost_explorer.rs
+++ src/cost_explorer.rs
@@ -1,16 +1,47 @@
 use chrono::{Date, Local, NaiveDate, TimeZone};
 use futures::executor::block_on;
 use rusoto_ce::{
-    CostExplorer, GetCostAndUsageRequest, GetCostAndUsageResponse, Group, GroupDefinition,
+    CostExplorerClient, GetCostAndUsageError, GetCostAndUsageRequest, GetCostAndUsageResponse,
+    Group, GroupDefinition,
 };
+
+use rusoto_core::{Region, RusotoError};
+
+use async_trait::async_trait;
 
 use crate::date_range::ReportDateRange;
 
-struct CostExplorerService<T: CostExplorer> {
+#[async_trait]
+trait GetCostAndUsage {
+    async fn get_cost_and_usage(
+        &self,
+        input: GetCostAndUsageRequest,
+    ) -> Result<GetCostAndUsageResponse, RusotoError<GetCostAndUsageError>>;
+}
+
+struct CostAndUsageClient(CostExplorerClient);
+
+impl CostAndUsageClient {
+    fn new(region: Region) -> Self {
+        CostAndUsageClient(CostExplorerClient::new(region))
+    }
+}
+
+#[async_trait]
+impl GetCostAndUsage for CostAndUsageClient {
+    async fn get_cost_and_usage(
+        &self,
+        input: GetCostAndUsageRequest,
+    ) -> Result<GetCostAndUsageResponse, RusotoError<GetCostAndUsageError>> {
+        self.get_cost_and_usage(input).await
+    }
+}
+
+struct CostExplorerService<T: GetCostAndUsage> {
     client: T,
     report_date_range: ReportDateRange,
 }
-impl<T: CostExplorer> CostExplorerService<T> {
+impl<T: GetCostAndUsage> CostExplorerService<T> {
     fn new(client: T, report_date_range: ReportDateRange) -> Self {
         CostExplorerService {
             client: client,
