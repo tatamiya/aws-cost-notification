40c4113429c0b92e1706401e9045e56f40038011
tatamiya <tamyuji1002@gmail.com>
2021-07-23 19:08:11 +0900

    change structure of notification message struct

--- src/message_builder.rs
+++ src/message_builder.rs
@@ -13,29 +13,19 @@ impl ParsedTotalCost {
 }
 
 struct NotificationMessage {
-    total_cost: ParsedTotalCost,
-    service_costs: Vec<ParsedServiceCost>,
+    header: String,
+    body: String,
 }
 impl NotificationMessage {
-    fn new(
-        total_cost: ParsedTotalCost,
-        service_costs: Vec<ParsedServiceCost>,
-    ) -> NotificationMessage {
+    fn new(total_cost: ParsedTotalCost, service_costs: Vec<ParsedServiceCost>) -> Self {
         NotificationMessage {
-            total_cost: total_cost,
-            service_costs: service_costs,
+            header: total_cost.to_message_header(),
+            body: service_costs
+                .iter()
+                .map(|x| x.to_message_line())
+                .collect::<Vec<_>>()
+                .join("\n"),
         }
-    }
-
-    fn build_header(self) -> String {
-        self.total_cost.to_message_header()
-    }
-    fn build_body(self) -> String {
-        self.service_costs
-            .iter()
-            .map(|x| x.to_message_line())
-            .collect::<Vec<_>>()
-            .join("\n")
     }
 }
 
@@ -45,36 +35,6 @@ mod test_build_message {
     use crate::cost_explorer::{Cost, ReportedDateRange};
     use chrono::{Local, TimeZone};
 
-    fn prepare_sample_message() -> NotificationMessage {
-        let sample_total_cost = ParsedTotalCost {
-            date_range: ReportedDateRange {
-                start_date: Local.ymd(2021, 7, 1),
-                end_date: Local.ymd(2021, 7, 11),
-            },
-            cost: Cost {
-                amount: 1.6234,
-                unit: "USD".to_string(),
-            },
-        };
-        let sample_service_costs = vec![
-            ParsedServiceCost {
-                service_name: "AWS CloudTrail".to_string(),
-                cost: Cost {
-                    amount: 0.0123,
-                    unit: "USD".to_string(),
-                },
-            },
-            ParsedServiceCost {
-                service_name: "AWS Cost Explorer".to_string(),
-                cost: Cost {
-                    amount: 0.182345,
-                    unit: "USD".to_string(),
-                },
-            },
-        ];
-
-        NotificationMessage::new(sample_total_cost, sample_service_costs)
-    }
     #[test]
     fn convert_total_cost_into_message_header_correctly() {
         let sample_total_cost = ParsedTotalCost {
@@ -108,18 +68,45 @@ mod test_build_message {
     }
 
     #[test]
-    fn build_header_correctly() {
-        let sample_message = prepare_sample_message();
-        let expected_header = "07/01~07/11の請求額は、1.62 USDです。";
-        let actual_header = sample_message.build_header();
-        assert_eq!(expected_header, actual_header);
-    }
+    fn construct_notification_message_correctly() {
+        let sample_total_cost = ParsedTotalCost {
+            date_range: ReportedDateRange {
+                start_date: Local.ymd(2021, 7, 1),
+                end_date: Local.ymd(2021, 7, 11),
+            },
+            cost: Cost {
+                amount: 1.6234,
+                unit: "USD".to_string(),
+            },
+        };
 
-    #[test]
-    fn build_body_correctly() {
-        let sample_message = prepare_sample_message();
-        let expected_body = "・AWS CloudTrail: 0.01 USD\n・AWS Cost Explorer: 0.18 USD";
-        let actual_body = sample_message.build_body();
-        assert_eq!(expected_body, actual_body);
+        let sample_service_costs = vec![
+            ParsedServiceCost {
+                service_name: "AWS CloudTrail".to_string(),
+                cost: Cost {
+                    amount: 0.0123,
+                    unit: "USD".to_string(),
+                },
+            },
+            ParsedServiceCost {
+                service_name: "AWS Cost Explorer".to_string(),
+                cost: Cost {
+                    amount: 0.182345,
+                    unit: "USD".to_string(),
+                },
+            },
+        ];
+
+        let actual_message = NotificationMessage::new(sample_total_cost, sample_service_costs);
+
+        assert_eq!(
+            "07/01~07/11の請求額は、1.62 USDです。",
+            actual_message.header,
+        );
+
+        assert_eq!(
+            "・AWS CloudTrail: 0.01 USD\n・AWS Cost Explorer: 0.18 USD",
+            actual_message.body,
+        );
     }
 }
