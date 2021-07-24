a998a4360b79d9dcd7b4b8f0db9dd321b7b8f58a
tatamiya <tamyuji1002@gmail.com>
2021-07-24 22:17:15 +0900

    clean: remove println for debug

--- src/main.rs
+++ src/main.rs
@@ -19,24 +19,18 @@ use tokio;
 
 #[tokio::main]
 async fn main() {
-    println!("Start Program");
     let reporting_date = Local::today();
     let report_date_range = ReportDateRange::new(reporting_date);
 
-    println!("Request Cost and Usage");
     // NOTE: Region must not be ap-northeast-1 because
     // because endpoint https://ce.ap-northeast1.amazonaws.com/ does not exist
     let cost_usage_client = CostAndUsageClient::new(Region::UsEast1);
     let cost_explorer = CostExplorerService::new(cost_usage_client, report_date_range);
-    println!("- Request Total Cost");
     let total_cost = cost_explorer.request_total_cost().await;
-    println!("- Request Service Cost");
     let service_costs = cost_explorer.request_service_costs().await;
 
-    println!("Build Notification Message");
     let notification_message = NotificationMessage::new(total_cost, service_costs);
 
-    println!("Send Result to Slack");
     let slack_client = SlackClient::new();
     let res = slack_notifier::send_message_to_slack(slack_client, notification_message);
     match res {
