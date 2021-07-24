eb0e4b0b97c5bb121b46dcb123a7d8ee4f8cd540
tatamiya <tamyuji1002@gmail.com>
2021-07-24 16:26:46 +0900

    add: test notification with reqwest in block kit style

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,39 @@ mod message_builder;
 mod slack_notifier;
 mod test_utils;
 
+use serde_json::json;
+
 fn main() {
-    println!("Hello, world!");
+    let channel = "#notification-test";
+    let text = json!(
+        {
+            "blocks": [
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "07/01~07/11の請求額は、1.62 USDです。"
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "fields": [
+                        {
+                            "type": "mrkdwn",
+                            "text": "・AWS CloudTrail: 0.01 USD\n・AWS Cost Explorer: 0.18 USD"
+                        }
+                    ]
+                }
+            ]
+        }
+    );
+
+    let res = slack_notifier::send_message_to_slack(text, channel);
+    match res {
+        Ok(_) => println!("Notification Successfully Completed!"),
+        Err(e) => panic!("Slack Notification Failed!: {}", e),
+    }
 }
--- src/slack_notifier.rs
+++ src/slack_notifier.rs
@@ -32,6 +32,35 @@ impl NotificationMessage {
     }
 }
 
+use dotenv::dotenv;
+use futures::executor::block_on;
+use std::result::Result;
+
+// extern crate slack_hook;
+
+use reqwest;
+//use tokio_compat_02::FutureExt;
+// use slack_hook::{Error, PayloadBuilder, Slack};
+
+pub fn send_message_to_slack(
+    message: JsonValue,
+    channel: &str,
+) -> Result<reqwest::blocking::Response, reqwest::Error> {
+    dotenv().ok();
+    let webhook_url = dotenv::var("SLACK_WEBHOOK_URL").expect("Webhook URL not found.");
+    let client = reqwest::blocking::Client::new();
+    let res = client.post(webhook_url).json(&message).send();
+    //    let slack = Slack::new(webhook_url.as_ref()).unwrap();
+    //    let p = PayloadBuilder::new()
+    //        .text(message.to_string())
+    //        .channel(channel)
+    //        .build()
+    //        .unwrap();
+    //
+    //    slack.send(&p)
+    res
+}
+
 #[cfg(test)]
 mod test_build_payload {
     use crate::message_builder::NotificationMessage;
