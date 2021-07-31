c6acfe83491b20b2e5921dd18925c36faa3c2504
tatamiya <tamyuji1002@gmail.com>
2021-07-31 18:19:05 +0900

    change: set timezone from env var.

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,9 @@ use date_range::ReportDateRange;
 use message_builder::NotificationMessage;
 use slack_notifier::SlackClient;
 
-use chrono::{Date, Local, TimeZone};
+use chrono::{Date, DateTime, Local, TimeZone};
 use chrono_tz::Tz;
+use dotenv::dotenv;
 use lambda_runtime::{handler_fn, Context, Error};
 use serde_json::Value;
 use std::error;
@@ -31,9 +32,10 @@ async fn lambda_handler(_: Value, _: Context) -> Result<(), Error> {
     let cost_usage_client = CostAndUsageClient::new();
     let slack_client = SlackClient::new();
 
-    let timezone: Tz = "Asia/Tokyo".parse().unwrap();
+    dotenv().ok();
+    let tz_string = dotenv::var("REPORTING_TIMEZONE").expect("REPORTING_TIMEZONE not found");
     let now = Local::now();
-    let reporting_date = now.with_timezone(&timezone).date();
+    let reporting_date = date_in_specified_timezone(now, tz_string).unwrap();
 
     println!(
         "Launched lambda handler with reporting date {}",
@@ -72,5 +74,61 @@ where
             Ok(())
         }
         Err(e) => Err(format!("Slack Notification Failed!: {}", e).into()),
+    }
+}
+
+fn date_in_specified_timezone<T: TimeZone>(
+    datetime: DateTime<T>,
+    tz_string: String,
+) -> Result<Date<Tz>, Box<dyn error::Error>> {
+    let timezone: Result<Tz, _> = tz_string.parse();
+    match timezone {
+        Ok(timezone) => Ok(datetime.with_timezone(&timezone).date()),
+        Err(e) => Err(format!("Invalid Timezone!: {}", e).into()),
+    }
+}
+
+#[cfg(test)]
+mod test_reporting_date {
+    use super::*;
+    use chrono::{Local, TimeZone, Utc};
+
+    #[test]
+    fn convert_timezone_correctly() {
+        let input_datetime = Local
+            .datetime_from_str("2021-07-31 12:00:00 UTC", "%Y-%m-%d %H:%M:%S %Z")
+            .unwrap();
+
+        let tz_string = "Asia/Tokyo".to_string();
+
+        let actual_date = date_in_specified_timezone(input_datetime, tz_string).unwrap();
+
+        assert_eq!("2021-07-31JST", format!("{}", actual_date));
+    }
+
+    #[test]
+    fn with_different_date() {
+        let input_datetime = Utc
+            .datetime_from_str("2021-07-31 15:00:00", "%Y-%m-%d %H:%M:%S")
+            .unwrap();
+
+        let tz_string = "Asia/Tokyo".to_string();
+
+        let actual_date = date_in_specified_timezone(input_datetime, tz_string).unwrap();
+
+        assert_eq!("2021-08-01JST", format!("{}", actual_date));
+    }
+
+    #[test]
+    fn return_error_for_invalid_timezone() {
+        let input_datetime = Local
+            .datetime_from_str("2021-07-31 15:05:00 UTC", "%Y-%m-%d %H:%M:%S %Z")
+            .unwrap();
+
+        let tz_string = "Invalid/Timezone".to_string();
+
+        let actual_date = date_in_specified_timezone(input_datetime, tz_string);
+
+        assert!(actual_date.is_err());
     }
 }
