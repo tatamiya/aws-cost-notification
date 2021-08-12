aee0e0f749ff2926c0119e86511220db4f1d0569
tatamiya <tamyuji1002@gmail.com>
2021-08-12 23:24:13 +0900

    add comments to message builder

--- src/message_builder.rs
+++ src/message_builder.rs
@@ -2,12 +2,30 @@ use crate::cost_explorer::cost_response_parser::{Cost, ReportedDateRange, Servic
 use chrono::Datelike;
 use std::fmt;
 
+/// # Example
+///
+/// ```
+/// let input_cost = Cost {
+///     amount: 132.2345,
+///     unit: "USD".to_string(),
+/// };
+/// assert_eq!("132.23 USD", format!("{}", input_cost));
+/// ```
 impl fmt::Display for Cost {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:.2} {}", self.amount, self.unit)
     }
 }
 
+/// # Example
+///
+/// ```
+/// let sample_date_range = ReportedDateRange {
+///     start_date: Local.ymd(2021, 7, 1),
+///     end_date: Local.ymd(2021, 7, 23),
+/// };
+/// assert_eq!("07/01~07/23", format!("{}", sample_date_range))
+/// ```
 impl fmt::Display for ReportedDateRange {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
@@ -22,22 +40,60 @@ impl fmt::Display for ReportedDateRange {
 }
 
 impl ServiceCost {
+    /// # Example
+    ///
+    /// ```
+    /// let sample_service_cost = ServiceCost {
+    ///     service_name: "AWS CloudTrail".to_string(),
+    ///     cost: Cost {
+    ///         amount: 0.0123,
+    ///         unit: "USD".to_string(),
+    ///     },
+    /// };
+    /// let actual_line = sample_service_cost.to_message_line();
+    ///
+    /// assert_eq!("・AWS CloudTrail: 0.01 USD", actual_line);
+    /// ```
     fn to_message_line(&self) -> String {
         format!("・{}: {}", self.service_name, self.cost)
     }
 }
 
 impl TotalCost {
+    /// # Example
+    ///
+    /// ```
+    /// let sample_total_cost = TotalCost {
+    ///     date_range: ReportedDateRange {
+    ///         start_date: Local.ymd(2021, 7, 1),
+    ///         end_date: Local.ymd(2021, 7, 11),
+    ///     },
+    ///     cost: Cost {
+    ///         amount: 1.6234,
+    ///         unit: "USD".to_string(),
+    ///     },
+    /// };
+    /// let actual_header = sample_total_cost.to_message_header();
+    ///
+    /// assert_eq!("07/01~07/11の請求額は、1.62 USDです。", actual_header);
+    /// ```
     fn to_message_header(&self) -> String {
         format!("{}の請求額は、{}です。", self.date_range, self.cost)
     }
 }
 
+/// Cost notification message to send to Slack.
 pub struct NotificationMessage {
+    /// Headline message to display the total cost
     pub header: String,
+    /// Body of the message to display costs for each service
     pub body: String,
 }
 impl NotificationMessage {
+    /// Build Slack notification message from parsed total cost and service costs.
+    ///
+    /// The service costs are displayed in descending order by amount,
+    /// skipping services which are less than 0.01 USD.
     pub fn new(total_cost: TotalCost, service_costs: Vec<ServiceCost>) -> Self {
         let mut sorted_service_costs = service_costs.clone();
         sorted_service_costs.sort_by(|a, b| b.cost.partial_cmp(&a.cost).unwrap());
